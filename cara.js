825a881d6796b672bbe2baf06e9375ff82c65dbd
Saveliy113 <sava2698@gmail.com>
2023-03-21 12:41:17 +0600

    Console.logs removed from js file, changed banner source in css

--- cara.js
+++ cara.js
@@ -1,17 +1,13 @@
-const bar = document.getElementById('bar')
-const nav = document.getElementById('navbar')
-const closeNav = document.getElementById('close') 
-console.log(bar)
-if(bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.toggle('active')
-    })
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const closeNav = document.getElementById('close');
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
 }
-if(closeNav) {
-    closeNav.addEventListener('click', () => {
-        nav.classList.remove('active')
-    })
+if (closeNav) {
+  closeNav.addEventListener('click', () => {
+    nav.classList.remove('active');
+  });
 }
-
-console.log('Hello for GIT')
-
